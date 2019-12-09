227e1ec80b4114374f22f56361cd8b76e9f23e41
xli.gem <x.li.gem@gmail.com>
2019-12-09 18:48:58 +0800

    perf: Add more test

--- src/generator/__tests__/CodeGenerator.spec.ts
+++ src/generator/__tests__/CodeGenerator.spec.ts
@@ -2,6 +2,9 @@ import { CodeGenerator } from '../CodeGenerator'
 import { Block } from '@/core'
 import { BlockTextField, BlockSlotField } from '@/fields'
 
+const spyWarn = jest.spyOn(global.console, 'warn')
+const spyTrace = jest.spyOn(global.console, 'trace')
+
 describe('CodeGenerator', () => {
   let generator: CodeGenerator
   let block: Block
@@ -20,9 +23,20 @@ describe('CodeGenerator', () => {
     field.value('this is a text')
     block.addField(field)
 
-    const code = generator.getFieldCode(block, 'test')
+    const sField = new BlockSlotField('sfield')
+    block.addField(sField)
+
+    let code = generator.getFieldCode(block, 'test')
 
     expect(code).toBe(`this is a text`)
+
+    spyWarn.mockReset()
+    code = generator.getFieldCode(block, 'sfield')
+    expect(spyWarn).toBeCalledTimes(1)
+
+    spyWarn.mockReset()
+    code = generator.getFieldCode(block, 'nothing')
+    expect(spyWarn).toBeCalledTimes(1)
   })
 
   it('getSlotFieldCode', () => {
@@ -37,14 +51,23 @@ describe('CodeGenerator', () => {
 
     const slotBlock = new Block({ name: 'slotBlock' })
     const slotField = new BlockSlotField('slotField')
-
     slotBlock.addField(slotField)
-
     block.connectToField(slotField)
 
-    const code = generator.getBlockCode(slotBlock)
-
+    let code = generator.getBlockCode(slotBlock)
     expect(code).toBe(['if () {', '  new Time()', '}'].join('\n'))
+
+
+    const field = new BlockTextField('field')
+    block.addField(field)
+
+    spyWarn.mockReset()
+    code = generator.getSlotFieldCode(block, 'field')
+    expect(spyWarn).toBeCalledTimes(1)
+
+    spyWarn.mockReset()
+    code = generator.getSlotFieldCode(block, 'nothing')
+    expect(spyWarn).toBeCalledTimes(1)
   })
 
   it('getBlockCode', () => {
@@ -55,6 +78,16 @@ describe('CodeGenerator', () => {
     const code = generator.getBlockCode(block)
 
     expect(code).toBe(`new Time()`)
+
+    const b = new Block({ name: 'nothing' })
+
+    spyWarn.mockReset()
+    spyTrace.mockReset()
+    const bCode = generator.getBlockCode(b)
+    expect(bCode).toBe('')
+
+    expect(spyWarn).toBeCalledTimes(1)
+    expect(spyTrace).toBeCalledTimes(1)
   })
 
   it('getTopBlockCode', () => {
