1f6031bf8735c59a331b8f6f364439a8eef2ec6c
xli.gem <x.li.gem@gmail.com>
2019-12-09 18:30:35 +0800

    feat: D tool for Path

--- src/renderer/__tests__/lib/Path.spec.ts
+++ src/renderer/__tests__/lib/Path.spec.ts
@@ -10,4 +10,54 @@ describe('Path', () => {
   it('new', () => {
     expect(path.dom.tagName.toUpperCase()).toBe('PATH')
   })
+
+  it('d', () => {
+    path.d
+      .M(10, 10)
+      .L(20, 20)
+      .l(20, 20)
+      .z()
+
+    expect(path.attr('d')).toBe('M 10,10 L 20,20 l 20,20 z')
+
+    path.d.clear()
+    path.d.done()
+    expect(path.attr('d')).toBe('')
+
+    path.d
+      .m(10, 10)
+      .v(10, 10)
+      .V(20)
+      .H(3)
+      .h(20, 20)
+      .Z()
+    expect(path.attr('d')).toBe('m 10,10 v 10 10 V 20 H 3 h 20 20 Z')
+
+    path.d.clear()
+    path.d
+      .m(10, 10)
+      .Q(10, 10, 20, 20)
+      .T(10, 20)
+      .q(10, 10, 20, 20, 30, 30)
+      .t(10, 20, 30)
+      .done()
+    expect(path.attr('d')).toBe('m 10,10 Q 10,10 20,20 T 10,20 q 10,10 20,20 t 10,20')
+
+    path.d.clear()
+    path.d
+      .m(0, 0)
+      .C(10, 10, 20, 20, 30, 30)
+      .S(20, 20, 40, 40)
+      .c(10, 10, 20, 20, 30, 30)
+      .s(20, 20, 40, 40)
+      .done()
+    expect(path.attr('d')).toBe('m 0,0 C 10,10 20,20 30,30 S 20,20 40,40 c 10,10 20,20 30,30 s 20,20 40,40')
+
+    path.d.clear()
+    path.d
+      .A(10, 10, 30, 0, 1, 20, 20)
+      .a(20, 20, 30, 1, 0, 50, 50)
+      .Z()
+    expect(path.attr('d')).toBe('A 10 10 30 0 1 20,20 a 20 20 30 1 0 50,50 Z')
+  })
 })
--- src/renderer/lib/Path.ts
+++ src/renderer/lib/Path.ts
@@ -1,8 +1,208 @@
 import { SElement } from './SElement'
 import { createSVGEl } from '../utils'
 
+class D {
+  el: SElement<SVGPathElement>
+  path: (string | number)[]
+
+  constructor(el: SElement<SVGPathElement>) {
+    this.el = el
+    this.clear()
+  }
+
+  pushCoords(coords: number[]) {
+    const step = 2
+
+    for (let i = 0; i < coords.length; i += step) {
+      if (i + step > coords.length) {
+        break
+      }
+
+      this.path.push(`${coords[i]},${coords[i + 1]}`)
+    }
+  }
+
+  d(cmd: string, params: number[], pairCount = 1) {
+    this.path.push(cmd)
+
+    for (let i = 0; i < params.length; i += pairCount) {
+      if (i + pairCount > params.length) {
+        break
+      }
+
+      const partParams = params.slice(i, i + pairCount)
+      const isCoords = pairCount % 2 === 0
+
+      if (isCoords) {
+        this.pushCoords(partParams)
+      } else {
+        this.path.push(...partParams)
+      }
+    }
+
+    return this
+  }
+
+  clear() {
+    this.path = []
+  }
+
+  done() {
+    const p = this.path.join(' ')
+
+    this.el.attr('d', p)
+  }
+
+  /**
+   * Horizontal line to (x,y)+
+   */
+  H(...lines: number[]) {
+    return this.d('H', lines)
+  }
+
+  /**
+   * Horizontal line to (dx,dy)+
+   */
+  h(...lines: number[]) {
+    return this.d('h', lines)
+  }
+
+  /**
+   * Vertical line to (x,y)+
+   */
+  V(...lines: number[]) {
+    return this.d('V', lines)
+  }
+
+  /**
+   * Vertical line to (dx,dy)+
+   */
+  v(...lines: number[]) {
+    return this.d('v', lines)
+  }
+
+  /**
+   * Move to (x,y)+
+   */
+  M(...coords: number[]) {
+    return this.d('M', coords, 2)
+  }
+
+  /**
+   * Move to (dx,dy)+
+   */
+  m(...coords: number[]) {
+    return this.d('m', coords, 2)
+  }
+
+  /**
+   * Line to (x,y)+
+   */
+  L(...coords: number[]) {
+    return this.d('L', coords, 2)
+  }
+
+  /**
+   * Line to (dx,dy)+
+   */
+  l(...coords: number[]) {
+    return this.d('l', coords, 2)
+  }
+
+  /**
+   * Smooth Quadratic Bezier Curve (x,y)+
+   */
+  T(...coords: number[]) {
+    return this.d('T', coords, 2)
+  }
+
+  /**
+   * Smooth Quadratic Bezier Curve (dx,dy)+
+   */
+  t(...coords: number[]) {
+    return this.d('t', coords, 2)
+  }
+
+  /**
+   * Smooth Cubic Bezier Curve (x2,y2, x,y)+
+   */
+  S(...coords: number[]) {
+    return this.d('S', coords, 4)
+  }
+
+  /**
+   * Smooth Cubic Bezier Curve (dx2,dy2, dx,dy)+
+   */
+  s(...coords: number[]) {
+    return this.d('s', coords, 4)
+  }
+
+  /**
+   * Quadratic Bezier Curve (x1,y1, x,y)+
+   */
+  Q(...coords: number[]) {
+    return this.d('Q', coords, 4)
+  }
+
+  /**
+   * Quadratic Bezier Curve (dx1,dy1, dx,dy)+
+   */
+  q(...coords: number[]) {
+    return this.d('q', coords, 4)
+  }
+
+  /**
+   * Cubic Bezier Curve (x1,y1, x2,y2, x,y)+
+   */
+  C(...coords: number[]) {
+    return this.d('C', coords, 6)
+  }
+
+  /**
+   * Cubic Bezier Curve (dx1,dy1, dx2,dy2, dx,dy)+
+   */
+  c(...coords: number[]) {
+    return this.d('c', coords, 6)
+  }
+
+  /**
+   * Elliptical Arc Curve (rx ry angle large-arc-flag sweep-flag x y)+
+   */
+  A(rx: number, ry: number, angle: number, largeArcFlag: 0 | 1, sweepFlag: 0 | 1, x: number, y: number) {
+    this.path.push('A', rx, ry, angle, largeArcFlag, sweepFlag, `${x},${y}`)
+    return this
+  }
+
+  /**
+   * Elliptical Arc Curve (rx ry angle large-arc-flag sweep-flag dx dy)+
+   */
+  a(rx: number, ry: number, angle: number, largeArcFlag: 0 | 1, sweepFlag: 0 | 1, dx: number, dy: number) {
+    this.path.push('a', rx, ry, angle, largeArcFlag, sweepFlag, `${dx},${dy}`)
+    return this
+  }
+
+  /**
+   * ClosePath
+   */
+  Z() {
+    this.d('Z', [])
+    this.done()
+  }
+
+  /**
+   * ClosePath
+   */
+  z() {
+    this.d('z', [])
+    this.done()
+  }
+}
+
 export class Path extends SElement<SVGPathElement> {
+  d: D
+
   constructor() {
     super(createSVGEl('path'))
+    this.d = new D(this)
   }
 }
