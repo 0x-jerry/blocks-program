8e304d30ef1695499bc994ccb1954a1da368d9cb
xli.gem <x.li.gem@gmail.com>
2019-12-10 18:38:22 +0800

    feat: Event Emitter

---
+++ src/shared/EventEmitter.ts
@@ -0,0 +1,43 @@
+import { SArray } from './utils'
+
+export interface IEventCallback {
+  (...params: any[]): void
+}
+
+export interface IEventsMap {
+  [name: string]: IEventCallback
+}
+
+export class EventEmitter<T extends IEventsMap = IEventsMap> {
+  protected _events: {
+    [name: string]: SArray<IEventCallback>
+  }
+
+  constructor() {
+    this._events = {}
+  }
+
+  events(type: string) {
+    if (!this._events[type]) {
+      this._events[type] = new SArray()
+    }
+
+    return this._events[type]
+  }
+
+  on<K extends keyof T>(type: K, func: T[K]) {
+    this.events(type as string).pushDistinct(func)
+  }
+
+  off<K extends keyof T>(type: K, func: T[K]) {
+    this.events(type as string).removeItem(func)
+  }
+
+  emit<K extends keyof T>(type: K, ...args: Parameters<T[K]>) {
+    this.events(type as string).forEach((func) => func(...args))
+  }
+
+  clear() {
+    this._events = {}
+  }
+}
---
+++ src/shared/__tests__/EventEmitter.spec.ts
@@ -0,0 +1,43 @@
+import { EventEmitter } from '../EventEmitter'
+
+describe('EventEmitter', () => {
+  let e: EventEmitter
+
+  beforeEach(() => {
+    e = new EventEmitter()
+  })
+
+  it('on', () => {
+    const fn = jest.fn()
+    e.on('test', fn)
+
+    const evts = e.events('test')
+    expect(evts[0]).toBe(fn)
+  })
+
+  it('off', () => {
+    const fn = () => {}
+
+    e.on('testoff', fn)
+    expect(e.events('testoff')[0]).toBe(fn)
+
+    e.off('testoff', fn)
+    const evts = e.events('testoff')
+    expect(evts.length).toBe(0)
+  })
+
+  it('emit', (done) => {
+    const fn = jest.fn()
+    e.on('test', fn)
+
+    e.emit('test')
+    expect(fn).toBeCalledTimes(1)
+
+    e.on('test1', (p) => {
+      expect(p).toBe(1)
+      done()
+    })
+
+    e.emit('test1', 1)
+  })
+})
