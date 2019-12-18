8b4e9dd31b8ec7b475151c8bd7b2e9b9ce665b0f
xli.gem <x.li.gem@gmail.com>
2019-12-18 16:33:10 +0800

    chore: Exclude test file when generate coverage

--- jest.config.js
+++ jest.config.js
@@ -7,7 +7,7 @@ module.exports = {
   },
   coverageDirectory: 'coverage',
   coverageReporters: ['html', 'lcov', 'text'],
-  collectCoverageFrom: ['src/**/*.ts'],
+  collectCoverageFrom: ['src/**/*.ts', '!**/index.ts', '!**/__tests__/*'],
   watchPathIgnorePatterns: ['/node_modules/'],
   moduleFileExtensions: ['ts', 'js'],
   rootDir: __dirname,
