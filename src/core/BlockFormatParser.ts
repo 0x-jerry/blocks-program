929c965fb8b366b7ef6a3362e988705ec6bb0dd7
xli.gem <x.li.gem@gmail.com>
2019-12-19 18:11:47 +0800

    perf: Block format parser.

--- src/core/BlockFormatParser.ts
+++ src/core/BlockFormatParser.ts
@@ -1,1 +1,10 @@
-export default {}
+import { ObjectAny } from '@/typedef'
+import { Block } from './Block'
+
+class BlockJSONParser {
+  parse(json: ObjectAny): Block | null {
+    return null
+  }
+}
+
+export const blockJSONParser = new BlockJSONParser()
