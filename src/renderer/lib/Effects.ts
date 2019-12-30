72add29cece86ed992da1c7a465abeac773c95a2
xli.gem <x.li.gem@gmail.com>
2019-12-30 18:08:41 +0800

    chore: SVG drop shadow effect

---
+++ src/renderer/lib/Effects.ts
@@ -0,0 +1,28 @@
+import { BasicElement } from './SElement'
+import { createSVGEl } from '../utils'
+import { uuid } from '@/shared'
+
+/**
+ * @todo Refactor
+ */
+export class DropShadowEffect extends BasicElement<SVGFilterElement> {
+  readonly id: string
+
+  constructor(id = uuid()) {
+    super(createSVGEl('filter'))
+    this.id = id
+    this.attr('id', this.id)
+
+    this.dom.innerHTML = `
+  <feGaussianBlur in="SourceAlpha" stdDeviation="3"/> 
+  <feOffset dx="2" dy="2" result="offsetblur"/> 
+  <feComponentTransfer>
+    <feFuncA type="linear" slope="0.5"/> 
+  </feComponentTransfer>
+  <feMerge> 
+    <feMergeNode/> 
+    <feMergeNode in="SourceGraphic"/> 
+  </feMerge>
+    `
+  }
+}
