e701c6979ce99d3957f664a28a44b5296fff6c2e
xli.gem <x.li.gem@gmail.com>
2020-01-10 22:52:28 +0800

    test: Add Connection test

---
+++ src/renderer/__tests__/Connection.spec.ts
@@ -0,0 +1,57 @@
+import { Connection, ConnectionType } from '../Connection'
+import { BlockSVG } from '../BlockSVG'
+import { Block, Workspace } from '@/core'
+import { Renderer } from '../Renderer'
+import './jest.setup'
+
+describe('Connection', () => {
+  let conn: Connection
+  let $r: Renderer
+
+  beforeEach(() => {
+    const block = new Block({ next: true, previous: true })
+    const w = new Workspace()
+    $r = new Renderer(w)
+
+    const b = new BlockSVG(block, $r)
+
+    conn = b.previousConnection!
+  })
+
+  it('setActive', () => {
+    conn.setActive(true)
+    expect(conn.sourceBlock.dom.classList.contains('s_conn_active')).toBe(true)
+    conn.setActive(false)
+    expect(conn.sourceBlock.dom.classList.contains('s_conn_active')).toBe(false)
+  })
+
+  it('checkDestConn', () => {
+    const b = new BlockSVG(new Block(), $r)
+
+    const bConn = new Connection(b, {
+      type: ConnectionType.blockNext,
+      acceptTypes: [ConnectionType.blockPrevious],
+      connectAction() {}
+    })
+    expect(conn.checkDestConn(bConn)).toBe(true)
+
+    const cConn = new Connection(b, {
+      type: ConnectionType.blockPrevious,
+      acceptTypes: [ConnectionType.blockNext],
+      connectAction() {}
+    })
+    expect(conn.checkDestConn(cConn)).toBe(false)
+  })
+
+  it('connectTo', () => {
+    const b = new BlockSVG(new Block({ next: true }), $r)
+
+    const action1 = jest.spyOn(conn, 'connectAction')
+    const action2 = jest.spyOn(b.nextConnection!, 'connectAction')
+
+    conn.connectTo(b.nextConnection || null)
+
+    expect(action1).toBeCalledTimes(1)
+    expect(action2).toBeCalledTimes(1)
+  })
+})
