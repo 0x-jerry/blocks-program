401942ef3005acba58bad7206f103e6bf8908efc
xli.gem <x.li.gem@gmail.com>
2020-01-10 22:27:34 +0800

    test: Add Renderer and WorkspaceSVG test

---
+++ src/renderer/__tests__/Renderer.spec.ts
@@ -0,0 +1,46 @@
+import { Renderer } from '../Renderer'
+import { Workspace } from '@/core'
+import './jest.setup'
+import { FieldSVG } from '../fields'
+
+describe('Renderer', () => {
+  let $r: Renderer
+
+  beforeEach(() => {
+    const w = new Workspace()
+    $r = new Renderer(w)
+  })
+
+  it('mount', () => {
+    const $div = document.createElement('div')
+    $r.mount($div)
+
+    expect($div.children[0]).toBe($r.svg.dom)
+  })
+
+  it('registerEffect', () => {
+    const testEffect = {
+      id: 'string',
+      dom: document.createElementNS('http://www.w3.org/2000/svg', 'pattern')
+    }
+
+    $r.registerEffect('test', testEffect)
+
+    expect($r.effects.test).toBe(testEffect)
+  })
+
+  it('registerFieldCtor', () => {
+    class TestField extends FieldSVG<any, any> {
+      constructor() {
+        // @ts-ignore
+        super()
+      }
+    }
+
+    $r.registerFieldCtor('test', TestField)
+
+    const Ctor = $r.getFieldCtor('test')
+
+    expect(Ctor).toBe(TestField)
+  })
+})
---
+++ src/renderer/__tests__/WorkspaceSVG.spec.ts
@@ -0,0 +1,43 @@
+import { WorkspaceSVG } from '../WorkspaceSVG'
+import { Workspace, Block } from '@/core'
+import { Renderer } from '../Renderer'
+import './jest.setup'
+
+describe('WorkspaceSVG', () => {
+  let $w: WorkspaceSVG
+
+  beforeEach(() => {
+    const w = new Workspace()
+    $w = new WorkspaceSVG(w, new Renderer(w), 100, 100)
+  })
+
+  it('addBlock', () => {
+    let b = $w.addBlock('test')
+
+    expect(b).toBeUndefined()
+
+    const block = new Block({}, 'testId')
+
+    $w.$w.definedBlocks.add(block)
+
+    b = $w.addBlock('testId')
+
+    expect(b?.options.type).toBe('testId')
+    expect(b).not.toBe(block)
+    expect($w.blocks.length).toBe(1)
+  })
+
+  it('removeBlock', () => {
+    const block = new Block({}, 'testId')
+
+    $w.$w.definedBlocks.add(block)
+
+    let b = $w.addBlock('testId')!
+
+    $w.removeBlock('id')
+    expect($w.blocks.length).toBe(1)
+
+    $w.removeBlock(b)
+    expect($w.blocks.length).toBe(0)
+  })
+})
