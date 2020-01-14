b79b95f15d2833068fa953fbce794ea941bf56b7
xli.gem <x.li.gem@gmail.com>
2020-01-14 23:12:17 +0800

    feat: Add float weight utils

--- src/renderer/__tests__/utils.spec.ts
+++ src/renderer/__tests__/utils.spec.ts
@@ -1,4 +1,4 @@
-import { createSVGEl, Transform, Sizeable } from '../utils'
+import { createSVGEl, Transform, Sizeable, floatWeight } from '../utils'
 
 describe('utils', () => {
   it('createSVGEl', () => {
@@ -38,5 +38,30 @@ describe('utils', () => {
     size.update({ width: 20, height: 20 })
     expect(size.width).toBe(20)
     expect(size.height).toBe(20)
+  })
+
+  it('FloatWeight', () => {
+    const rendered = document.body.contains(floatWeight.dom)
+    expect(rendered).toBe(true)
+
+    const input = document.createElement('input')
+
+    floatWeight.append(input)
+    expect(floatWeight.dom.contains(input)).toBe(true)
+
+    floatWeight.replace()
+    expect(floatWeight.dom.contains(input)).toBe(false)
+
+    floatWeight.move(10, 20)
+    expect(floatWeight.x).toBe(10)
+    expect(floatWeight.y).toBe(20)
+
+    floatWeight.show()
+    expect(floatWeight.dom.classList.contains('s_float_weight_show')).toBe(true)
+    expect(floatWeight.dom.classList.contains('s_float_weight_hide')).toBe(false)
+
+    floatWeight.hide()
+    expect(floatWeight.dom.classList.contains('s_float_weight_hide')).toBe(true)
+    expect(floatWeight.dom.classList.contains('s_float_weight_show')).toBe(false)
   })
 })
---
+++ src/renderer/utils/FloatWeight.ts
@@ -0,0 +1,50 @@
+export class FloatWeight {
+  dom: HTMLDivElement
+
+  x: number
+  y: number
+
+  constructor() {
+    this.dom = document.createElement('div')
+
+    this._initRootDom()
+  }
+
+  private _initRootDom() {
+    this.dom.classList.add('s_float_weight')
+    document.body.append(this.dom)
+  }
+
+  show() {
+    this.dom.classList.remove('s_float_weight_hide')
+    this.dom.classList.add('s_float_weight_show')
+  }
+
+  hide() {
+    this.dom.classList.remove('s_float_weight_show')
+    this.dom.classList.add('s_float_weight_hide')
+  }
+
+  move(x: number, y: number) {
+    this.x = x
+    this.y = y
+
+    this.dom.style.left = `${x}px`
+    this.dom.style.top = `${y}px`
+  }
+
+  replace(...nodes: HTMLElement[]) {
+    this.dom.innerHTML = ''
+    this.dom.append(...nodes)
+  }
+
+  append(...nodes: HTMLElement[]) {
+    this.dom.append(...nodes)
+  }
+
+  destroy() {
+    this.dom.remove()
+  }
+}
+
+export const floatWeight = new FloatWeight()
