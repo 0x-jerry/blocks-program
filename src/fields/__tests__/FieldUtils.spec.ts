e1046cd7644288dffba986e1d9099731610c1875
Fantasy <x.li.gem@gmail.com>
2020-03-01 23:31:34 +0800

    test: add test for field utils

---
+++ src/fields/__tests__/FieldUtils.spec.ts
@@ -0,0 +1,23 @@
+import { FieldTypes } from '../const'
+import { fieldUtils } from '../FieldUtils'
+import { BlockField } from '@/core'
+
+describe('fieldUtils', () => {
+  it('default constructors', () => {
+    for (const type of Object.values(FieldTypes)) {
+      expect(fieldUtils.get(type)).toBeTruthy()
+    }
+  })
+
+  it('set', () => {
+    const newType = 'new-type'
+    class newField extends BlockField {
+      constructor(name: string, value: string, opt: any) {
+        super(name, value, newType, opt)
+      }
+    }
+
+    fieldUtils.set(newType, newField)
+    expect(fieldUtils.ctors[newType]).toBe(newField)
+  })
+})
